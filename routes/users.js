3c5238e934b4860d417102c24fe17592381fdce3
Shamil Saleem <shamilsaleemc@gmail.com>
2024-06-23 22:28:59 +0530

    Added signup and login pages

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.redirect("/");
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("users/login", { admin: false, login:true});
+});
+
+router.get("/signup", function (req, res, next) {
+  res.render("users/signup", { admin: false, login:true});
 });
 
 module.exports = router;
