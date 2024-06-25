680ff8bd350760679e201e0a8df43579c89da7ee
Shamil Saleem <shamilsaleemc@gmail.com>
2024-06-25 12:58:10 +0530

    MongoDB connection added

--- essentials/constants.js
+++ essentials/constants.js
@@ -1,5 +1,7 @@
 var constants ={
-    'project-name':'Shopping Cart'
+    'project-name':'Shopping Cart',
+    'database-uri':'mongodb://localhost:27017/',
+    'database-name':'shopping-cart'
 }
 
 module.exports = constants;
---
+++ essentials/database.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const db_name = require("./constants")["database-name"];
+const db_uri = require("./constants")["database-uri"];
+
+const client = new MongoClient(db_uri);
+
+const db = {
+  state: null,
+};
+
+//Database connection.
+async function run() {
+  await client.connect().then(() => {
+    console.log("You successfully connected to MongoDB!");
+    db.state = client;
+  });
+}
+
+module.exports.connect = run().then(() => {
+  module.exports.collection = {
+    products: db.state.db(db_name).collection("products"),
+    users: db.state.db(db_name).collection("users"),
+    orders: db.state.db(db_name).collection("users"),
+  };
+});
