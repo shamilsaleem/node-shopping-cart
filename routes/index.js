cddc71ab54a8c6dd8d742a11d98a54cbe9014129
Shamil Saleem <shamilsaleemc@gmail.com>
2024-06-27 22:25:43 +0530

    Displyed all products in the home screen and minor design change in the admin home page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path')
-var constants = require('../essentials/constants')
+var path = require("path");
+var constants = require("../essentials/constants");
+var productHelpers = require("../helpers/product-helpers");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('users/index', {title:constants['project-name']});
+router.get("/", function (req, res, next) {
+  productHelpers.getAllProducts().then((products) => {
+    res.render("users/index", { title: constants["project-name"], products });
+  });
 });
 
 module.exports = router;
